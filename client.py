7c31f8f99afc452ad820a7cea26a7f89e2e4297d
ViniciusBacelar <viniciusbacelar123@outlook.com>
2020-05-25 15:43:53 -0300

    att 1.0.0

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 import socket
+from endereco_ip import endereco_ip
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
     mensagem_envio = input("Digite a mensagem: ")
-    cliente.sendto(mensagem_envio.encode(), ("192.168.15.149", 12000))
+    cliente.sendto(mensagem_envio.encode(), (endereco_ip, 12000))
     mensagem_bytes, endereco_ip_servidor = cliente.recvfrom(2048)
     print(mensagem_bytes.decode())
